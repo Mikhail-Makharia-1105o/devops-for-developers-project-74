5bc7c972fa1e074679b69c6ba57f35f4f7de1dd0
SHRURUvevo <mmikhail602@gmail.com>
2026-03-28 21:16:02 +0300

    FIX: Database integration

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -5,14 +5,17 @@ module.exports = {
     dialect: 'sqlite',
     storage: './database.sqlite',
   },
-  production: {
-    dialect: 'postgres',
-    database: process.env.DATABASE_NAME,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    host: process.env.DATABASE_HOST,
-  },
+// ...
+production: {
+  dialect: 'postgres',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: process.env.DATABASE_PORT || 5432,
+  database: process.env.DATABASE_NAME || 'postgres',
+  username: process.env.DATABASE_USERNAME || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
+},
+// ...
+
   test: {
     dialect: 'sqlite',
     storage: './database.test.sqlite',
