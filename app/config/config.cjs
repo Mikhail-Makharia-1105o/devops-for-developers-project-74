8eb55f629081c71acf0a3e31e9f9d87e4a48cc77
SHRURUvevo <mmikhail602@gmail.com>
2026-03-28 21:51:13 +0300

    ADD: Postre integration

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -5,27 +5,20 @@ module.exports = {
     dialect: 'sqlite',
     storage: './database.sqlite',
   },
-// ...
-production: {
-  dialect: 'postgres',
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: process.env.DATABASE_PORT || 5432,
-  database: process.env.DATABASE_NAME || 'postgres',
-  username: process.env.DATABASE_USERNAME || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'postgres',
-},
-// ...
-
-  test: {
-    dialect: 'sqlite',
-    storage: './database.test.sqlite',
+  production: {
+    dialect: 'postgres',
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: process.env.DATABASE_PORT || 5432,
+    database: process.env.DATABASE_NAME || 'postgres',
+    username: process.env.DATABASE_USERNAME || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'postgres',
   },
-  // test: {
-  //   dialect: 'postgres',
-  //   database: process.env.DATABASE_NAME,
-  //   username: process.env.DATABASE_USERNAME,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   port: process.env.DATABASE_PORT,
-  //   host: process.env.DATABASE_HOST,
-  // },
+  test: {
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
+  },
 };
